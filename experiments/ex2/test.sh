780635db0bcb2722643aa8356b3a776198a6b87b
Emilio Tuosto <emilio@le.ac.uk>
2019-08-24 11:24:59 +0200

    small bug on par

--- experiments/ex2/test.sh
+++ experiments/ex2/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-../../gg2pom -d . global_view.sgg
-../../../ccpom/dirpoms.py --draw --graphml global_view cc2 cc3 proj
+../../gg2pom --gml -d . global_view.sgg
+~/Dropbox/mypapers/chosem/chosem-tools/dirpoms.py --draw --graphml global_view cc2 cc3 proj
 
