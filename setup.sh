f27095a824bd9225dfbf05da477ab9501910d4c4
Roberto Guanciale <robertog@gmail.com>
2019-11-22 16:47:51 +0100

    isertup file

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo apt-get install python3-pip graphviz ghc libghc-missingh-dev libghc-split-dev ipandoc libghc-hxt-dev python3-numpy python3-scipy
+make setup
+pip3 install networkx==2.3
+
