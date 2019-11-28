30123515a5f866fb8f78a03b65c315f6da200b44
Roberto Guanciale <robertog@gmail.com>
2019-11-28 16:57:35 +0100

    changed setupwq

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 sudo apt-get install python3-pip graphviz ghc libghc-missingh-dev libghc-split-dev ipandoc libghc-hxt-dev python3-numpy python3-scipy
 make setup
 pip3 install networkx==2.3
-
+echo "REady"
