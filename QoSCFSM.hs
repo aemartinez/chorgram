61995f76f78eb76a447fed5f580771626185c029
Agustin Eloy Martinez Sune <agusms@amazon.com>
2021-10-11 02:05:13 -0400

    stable version with smt lib integration, no actual smt solving yet

--- QoSCFSM.hs
+++ QoSCFSM.hs
@@ -13,10 +13,22 @@ import Misc
 import Data.Maybe
 import Data.Map.Strict as M
 import CFSM
+import SMTLIB as SMTLIB
 
 type QoSAttr = String
-type QoSSpec = Map QoSAttr Int -- ToDo: define QoSSpec as theory over real numbers
+type QoSSpec = (Set QoSAttr, SMTLIB.Term)
+    -- QoSSpec is one term whose only free symbols are names of QoS attributes.
+
+-- ToDo: 
+-- type QoSSpec = (Set QoSAttr, SMTLIB.Script)
+--     -- Commands in the script are limited to 'declare-fun', 'declare-const', 'define-fun', 'assert'
+--     -- Any undefined symbol appearing in a term will be considered a QoSAttr and has to be part of the set of QoSAttr of the QoSSystem.
 
 type QoSMap = Map State QoSSpec
 type QoSSystem  = ( System , Set QoSAttr, Map Ptp QoSMap )
 -- (Map Ptp QoSMap) should be consistent with the states in Ptp
+-- Each QoSSpec in QoSMap should have the same set of QoSAttr as the QoSSystem.
+
+unionOfSpecs :: Set QoSSpec -> QoSSpec
+unionOfSpecs = S.foldl acum (S.empty, SMTLIB.smtTrue)
+    where acum = (\(attrSet, trm) (attrSet', trm') -> (S.union attrSet attrSet', SMTLIB.smtAND trm trm'))
--- QoSLogic.hs
+++ QoSLogic.hs
@@ -19,6 +19,8 @@ import QoSCFSM
 import PomsetSemantics
 import CFSM
 import TS as TS
+import qualified SMTLIB
+import qualified SMTLIB.Solver
 
 data QL = T
     | F
@@ -32,7 +34,9 @@ isSatisfiable qossys@(sys, qosattrs, qosmaps) prop run conf =
     case prop of 
         T -> True
         F -> False
-        Spec phi -> smtsolve (aggregation qossys run) phi
+        Spec phi -> checkEntailment (aggregation qossys run) phi
+            where checkEntailment p@(attrs, term) p'@(attrs', term') = not (SMTLIB.Solver.isSat smtScript)
+                    where smtScript = buildScript (S.union attrs attrs') (SMTLIB.smtAND term (SMTLIB.smtNOT term'))
         Not psi -> not (isSatisfiable qossys psi run conf)
         Or psi psi' -> (isSatisfiable qossys psi run conf) || (isSatisfiable qossys psi' run conf)
         Until psi gchor psi' -> helper
@@ -42,7 +46,7 @@ isSatisfiable qossys@(sys, qosattrs, qosmaps) prop run conf =
                           where psiHolds = isSatisfiable qossys psi run conf
                                 nextStepHolds = S.member True (S.map filterGChorAndRecursion nextconfs) 
                                 -- can we use forall or exists ?
-                                -- we are not implementing short circuit to discard traces not in gchor
+                                -- we are not i mplementing short circuit to discard traces not in gchor
                                 -- we could do this more efficiently by returning true as soon as one extension suceeds
                                 filterGChorAndRecursion (kevt@(s1, s2, ptp1, ptp2, dir, msg), conf') = 
                                         S.member (traceOf extendedRun) language && recursiveCheck extendedRun
@@ -61,33 +65,42 @@ traceOf = L.map actionOf
     where actionOf (_, (s1, s2, ptp1, ptp2, dir, msg), _) = ((ptp1, ptp2), dir, msg)
 
 aggregation :: QoSSystem -> Run -> QoSSpec
-aggregation qossys run = aggregateSpecs (collectSpecs qossys run)
+aggregation (sys, qosattrs, qosmaps) run = unionOfSpecs $ S.insert (aggregationAxioms qosattrs ptpStateSet) (collectLocalSpecs qosmaps ptpStateSet)
+    where ptpStateSet = (getStatesFromRun sys run)
 
-collectSpecs :: QoSSystem -> Run -> Set QoSSpec 
-collectSpecs (sys, qosattrs, qosmaps) run = S.map (getQoSSpec qosmaps) (getStatesFromRun sys run)
+collectLocalSpecs :: Map Ptp QoSMap -> Set (Ptp, State) -> Set QoSSpec 
+collectLocalSpecs qosmaps ptpStateSet = S.map (getQoSSpec qosmaps) ptpStateSet
 
-getQoSSpec :: Map Ptp QoSMap -> (State, Ptp) -> QoSSpec
-getQoSSpec qosmaps (state, ptp) = fromJust $ M.lookup state $ fromJust (M.lookup ptp qosmaps)
+getQoSSpec :: Map Ptp QoSMap -> (Ptp, State) -> QoSSpec
+getQoSSpec qosmaps (ptp, state) = renameAttrs ptp state qosspec
+    where qosspec = fromJust $ M.lookup state $ fromJust (M.lookup ptp qosmaps)
 
-getStatesFromRun :: System -> Run -> Set (State, Ptp)
+renameAttrs :: Ptp -> State -> QoSSpec -> QoSSpec
+renameAttrs ptp state spec@(attrSet, smtTerm) = (S.map f attrSet, SMTLIB.mapS f smtTerm)
+    where f s = if S.member s attrSet then s ++ sufix else s
+          sufix = "_" ++ ptp ++ "_" ++ state
+
+getStatesFromRun :: System -> Run -> Set (Ptp, State)
+-- getStatesFromRun s r 
+-- PRE: r is a valid run of system s
+-- POST: (ptp, st) is in result iff local state st of ptp appears in a configuration of run r.
 getStatesFromRun sys@(cfsms, ptps) run = 
     case run of [] -> (statesPtpsOf $ initConf sys) 
                 ktrans@(_, _, conf'):tailRun -> S.union (statesPtpsOf conf') (getStatesFromRun sys tailRun)
-        where statesPtpsOf conf@(states, buf) = M.foldrWithKey (\id ptp -> S.insert (states!!id, ptp)) S.empty ptps
+        where statesPtpsOf conf@(states, buf) = M.foldrWithKey (\id ptp -> S.insert (ptp, states!!id)) S.empty ptps
 
-aggregateSpecs :: Set QoSSpec -> QoSSpec
--- aggregateSpecs qosSpecSet
--- PRE qosSpecSet is not empty
-aggregateSpecs qosSpecSet
-    | (S.null qosSpecSet) = error "empty qosspecset"
-    | otherwise = S.foldl aggregateTwoSpecs (S.elemAt 0 qosSpecSet) (S.deleteAt 0 qosSpecSet)
+aggregationAxioms :: Set QoSAttr -> Set (Ptp, State) -> QoSSpec
+aggregationAxioms attrSet ptpStateSet = S.foldl aux (attrSet, SMTLIB.smtTrue) attrSet
+    where aux (atset, trm) attr = (S.union atset localAttrNames, SMTLIB.smtAND trm $ equalToCompositionTerm attr localAttrNames)
+            where localAttrNames = S.map (\(ptp, state) -> attr ++ "_" ++ ptp ++ "_" ++ state) ptpStateSet
 
--- from here on the implementation will be dependent on the integration with smt solver
+equalToCompositionTerm :: QoSAttr -> Set QoSAttr -> SMTLIB.Term
+equalToCompositionTerm attr attrSet = SMTLIB.smtEq (SMTLIB.smtConstName attr) compositionTerm
+    where compositionTerm = S.foldl writeSum firstAttrTrm (S.deleteAt 0 attrSet)
+            where writeSum = (\trm att -> SMTLIB.smtPlus trm (SMTLIB.smtConstName att))
+                  firstAttrTrm = (SMTLIB.smtConstName $ S.elemAt 0 attrSet)
 
-aggregateTwoSpecs :: QoSSpec -> QoSSpec -> QoSSpec
-aggregateTwoSpecs spec spec' = M.mapWithKey (aggregateAttr spec) spec'
-    where aggregateAttr spec attr val = (fromJust (M.lookup attr spec)) + val
-
-smtsolve :: QoSSpec -> QoSSpec -> Bool
--- smtsolve phi phi' = True
-smtsolve phi phi' = L.all id (M.elems $ M.mapWithKey (\k v -> (fromJust $ M.lookup k phi) == v) phi')
+buildScript :: Set QoSAttr -> SMTLIB.Term -> SMTLIB.Script
+buildScript attrSet trm = SMTLIB.Script $ declareAttrs ++ [SMTLIB.Assert trm] ++ [SMTLIB.Check_sat]
+    where declareAttrs = S.foldl (\cmds attr -> (declareConst attr):cmds) [] attrSet
+          declareConst attr = SMTLIB.Declare_const attr (SMTLIB.Sort_identifier (SMTLIB.Identifier "Real"))
